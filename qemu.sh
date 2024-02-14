4229986fa80323ecf1ef550a9ae9c55461454cb4
bagggage <pigulevskiykostya@gmail.com>
2024-02-15 01:49:46 +0300

    Added uefi firmware binary

--- qemu.sh
+++ qemu.sh
@@ -3,7 +3,7 @@
 set -e
 . ./env.sh
 
-export UEFI=/usr/share/OVMF/OVMF-mouse-efi.fd
+export UEFI=${THIRD_PRT}/uefi/OVMF-efi.fd
 
 unset GTK_PATH
 qemu-system-x86_64 -bios ${UEFI} -smp cores=4 ${DIST_DIR}/bamos.iso
