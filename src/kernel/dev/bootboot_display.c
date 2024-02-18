1b2a2ed42ff8da75085a56fb84b28d78949438a8
bagggage <pigulevskiykostya@gmail.com>
2024-02-18 21:11:07 +0300

    Implemented display device based on data provided by 'bootboot' bootloader

---
+++ src/kernel/dev/bootboot_display.c
@@ -0,0 +1,28 @@
+#include "bootboot_display.h"
+
+#include <bootboot.h>
+
+extern BOOTBOOT bootboot; // see bootboot.h
+
+static Framebuffer display_fb;
+
+#define BOOTBOOT_FB_BPP 4
+
+Status init_bootboot_display(DisplayDevice* dev) {
+    if (bootboot_display_is_avail() == FALSE) {
+        return KERNEL_ERROR;
+    }
+
+    display_fb.base = (uint8_t*)bootboot.fb_ptr;
+    display_fb.width = bootboot.fb_width;
+    display_fb.height = bootboot.fb_height;
+    display_fb.scanline = bootboot.fb_scanline;
+    display_fb.format = (FbFormat)bootboot.fb_type;
+    display_fb.bpp = BOOTBOOT_FB_BPP;
+
+    return KERNEL_OK;
+}
+
+bool_t bootboot_display_is_avail() {
+    return (bootboot.fb_ptr != NULL && bootboot.fb_size != 0);
+}
---
+++ src/kernel/dev/bootboot_display.h
@@ -0,0 +1,4 @@
+#include "display.h"
+
+Status init_bootboot_display(DisplayDevice* dev);
+bool_t bootboot_display_is_avail();
