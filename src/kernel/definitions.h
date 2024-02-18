e214280ceaac49f96dcd11655cd4dd9f328e40d7
bagggage <pigulevskiykostya@gmail.com>
2024-02-18 21:07:54 +0300

    Added NULL, TRUE, FALSE, boot_t and stdint.h

--- src/kernel/definitions.h
+++ src/kernel/definitions.h
@@ -1,6 +1,25 @@
 #pragma once
 
+#include "stdint.h"
+
 // Kernel defenitions used in implementetion
+
+#ifndef NULL
+    #define NULL ((void*)0)
+#endif
+
+#ifdef TRUE
+    #undef TRUE
+#endif
+#ifdef FALSE
+    #undef FALSE
+#endif
+
+typedef uint8_t bool_t;
+typedef uint64_t size_t;
+
+#define TRUE 1
+#define FALSE 0
 
 // Result of the operation
 typedef enum Status {
