81e3e2cae2db9151aca11d221dcdfaa834c1c98b
bagggage <pigulevskiykostya@gmail.com>
2024-02-20 12:27:38 +0300

    Commented

--- src/kernel/io/logger.h
+++ src/kernel/io/logger.h
@@ -2,14 +2,23 @@
 
 #include "definitions.h"
 
+/*
+Raw kernel logger.
+Can be used only after display device initialization.
+*/
+
 typedef enum LogType {
     MSG,
     WARN,
     ERROR
 } LogType;
 
+// Prints log to display framebuffer
 void kernel_log(LogType log_type, const char* fmt, ...);
 
+// Prints msg log
 void kernel_msg(const char* fmt, ...);
+// Prints warning log
 void kernel_warn(const char* fmt, ...);
+// Prints error log
 void kernel_error(const char* fmt, ...);
