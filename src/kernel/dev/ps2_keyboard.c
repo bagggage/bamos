304988aea178ae0a35771979fbe827a933edf14e
bagggage <pigulevskiykostya@gmail.com>
2024-02-21 00:12:08 +0300

    Refactoring

--- src/kernel/dev/ps2_keyboard.c
+++ src/kernel/dev/ps2_keyboard.c
@@ -3,33 +3,29 @@
 #include "keyboard.h"
 #include "mem.h"
 
-uint8_t inb(unsigned short port) {
-    unsigned char ret;
+uint8_t inb(uint16_t port) {
+    uint8_t ret;
 
     asm volatile("in %%dx, %%al" : "=a"(ret) : "d"(port));
 
     return ret;
 }
 
-void outb(unsigned char value, unsigned short port) {
+void outb(uint8_t value, uint16_t port) {
     asm volatile("out %%al, %%dx" : : "a"(value), "d"(port));
 }
 
 Status init_ps2_keyboard(KeyboardDevice* keyboard_device) {
-    if (keyboard_device == NULL) {
-        return KERNEL_ERROR;
-    }
-    
+    if (keyboard_device == NULL) return KERNEL_ERROR;
+
     outb(PS2_PORT, SET_DEFAULT_PARAMETERS); 
 
-    if (inb(PS2_PORT) != ACK) {
-        return KERNEL_ERROR;
-    } 
+    if (inb(PS2_PORT) != ACK) return KERNEL_ERROR;
 
     keyboard_device->interface.get_scan_code = &ps2_get_scan_code;
 
     return KERNEL_OK;
-}   
+}
 
 uint8_t ps2_get_scan_code() {
     return inb(PS2_PORT);    
