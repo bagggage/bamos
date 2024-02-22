747d401ab9a8af94228da4cd8b46a5199a6d256d
bagggage <pigulevskiykostya@gmail.com>
2024-02-22 21:15:33 +0300

    Fixed outb function args invalid order, fixed reading scancodes issue

--- src/kernel/dev/ps2_keyboard.c
+++ src/kernel/dev/ps2_keyboard.c
@@ -1,5 +1,6 @@
 #include "ps2_keyboard.h"
 
+#include "io/logger.h"
 #include "keyboard.h"
 #include "mem.h"
 
@@ -11,22 +12,27 @@ uint8_t inb(uint16_t port) {
     return ret;
 }
 
-void outb(uint8_t value, uint16_t port) {
+void outb(uint16_t port, uint8_t value) {
     asm volatile("out %%al, %%dx" : : "a"(value), "d"(port));
 }
 
 Status init_ps2_keyboard(KeyboardDevice* keyboard_device) {
     if (keyboard_device == NULL) return KERNEL_ERROR;
 
-    outb(PS2_PORT, SET_DEFAULT_PARAMETERS); 
-
-    if (inb(PS2_PORT) != ACK) return KERNEL_ERROR;
+    //outb(PS2_COMMAND_PORT, SET_DEFAULT_PARAMETERS); 
+    if (inb(PS2_DATA_PORT) != ACK) return KERNEL_ERROR;
 
     keyboard_device->interface.get_scan_code = &ps2_get_scan_code;
 
     return KERNEL_OK;
 }
 
+uint8_t ignore_released = PS2_SCAN_CODE_NONE;
+
 uint8_t ps2_get_scan_code() {
-    return inb(PS2_PORT);    
+    PS2ScanCode scancode = PS2_SCAN_CODE_NONE;
+    
+    if (inb(PS2_STATUS_PORT) & 1) scancode = inb(PS2_DATA_PORT);
+
+    return scancode == PS2_SCAN_CODE_NONE ? SCAN_CODE_NONE : scancode;
 }
