2b99057985402b482821998a118f34e4ce99552f
bagggage <pigulevskiykostya@gmail.com>
2024-02-22 22:32:10 +0300

    Implemented single core thread for multicores cpus

--- src/kernel/init.c
+++ src/kernel/init.c
@@ -1,11 +1,14 @@
 #include "init.h"
 
 #include <bootboot.h>
+#include <cpuid.h>
 
 #include "dev/bootboot_display.h"
 #include "dev/keyboard.h"
 #include "dev/ps2_keyboard.h"
 #include "io/logger.h"
+
+#define CPUID_GET_FEATURE 1
 
 #ifdef MEM_RAW_PATCH
 Status init_memory() {
@@ -13,7 +16,27 @@ Status init_memory() {
 }
 #endif
 
+extern BOOTBOOT bootboot;
+
+void halt_logical_core() {
+    while (1);
+}
+
+void split_logical_cores() {
+    uint32_t eax, ebx, ecx, edx;
+
+    __get_cpuid(CPUID_GET_FEATURE, &eax, &ebx, &ecx, &edx);
+
+    // Get logical core ID (31-24 bit)
+    ebx = ebx >> 24;
+
+    // Only core with ID = 0 pass
+    if (ebx != 0) halt_logical_core();
+}
+
 Status init_kernel() {
+    split_logical_cores();
+
     // After this step we should be able to use memory allocations, otherwise drop kernel =)
     if (init_memory() != KERNEL_OK) return KERNEL_PANIC;
 
