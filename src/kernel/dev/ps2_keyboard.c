dc7ade2c2665e3614e38dcf85bde92c59e087501
bagggage <pigulevskiykostya@gmail.com>
2024-02-25 05:30:27 +0300

    In progress

--- src/kernel/dev/ps2_keyboard.c
+++ src/kernel/dev/ps2_keyboard.c
@@ -1,23 +1,12 @@
 #include "ps2_keyboard.h"
 
+#include "io/tty.h"
 #include "io/logger.h"
 #include "keyboard.h"
 #include "mem.h"
 
-uint8_t inb(uint16_t port) {
-    uint8_t ret;
-
-    asm volatile("in %%dx, %%al" : "=a"(ret) : "d"(port));
-
-    return ret;
-}
-
-void outb(uint16_t port, uint8_t value) {
-    asm volatile("out %%al, %%dx" : : "a"(value), "d"(port));
-}
-
 void wait() {
-    for (size_t i = 0; i < 0xFF000000; ++i) {
+    for (size_t i = 0; i < 1000; ++i) {
         asm volatile("");
     }
 }
