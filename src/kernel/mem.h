39f7dfd22a0a17b3582299b3e36c634d186d8992
bagggage <pigulevskiykostya@gmail.com>
2024-03-15 06:27:45 +0300

    Implemented kernel page tables initialization

--- src/kernel/mem.h
+++ src/kernel/mem.h
@@ -2,6 +2,8 @@
 
 #include "definitions.h"
 
+#include "cpu/paging.h"
+
 Status init_memory();
 
 // Kernel space memory allocation
@@ -9,12 +11,7 @@ void* kmalloc(size_t size);
 // Kernel space memory free
 void kfree(void* allocated_mem);
 
-/*
-Map physical memory pages to virtual.
-Never fails, so be carefull when using this function and always check if virtual addresses already taken
-or physcical memory was allocated before for other purposes.
-*/
-void map_phys_to_virt(uint64_t physcical_addr, uint64_t virtual_addr, uint64_t pages_count);
+void log_memory_page_tables(PageMapLevel4Entry* pml4);
 
 // Allocate pages in common heap
 void* alloc_pages(size_t count);
@@ -22,13 +19,21 @@ void* alloc_pages(size_t count);
 void free_pages(void* begin, size_t count);
 
 // Check if virtual address is mapped (but also can be not presented)
-bool_t is_virt_addr_mapped(uint64_t address);
+bool_t is_virt_addr_mapped(const uint64_t address);
 
 /*
 Returns physical address that mapped to virtual.
 If virtual address is not mapped returns 'INVALID_ADDRESS'.
 */
-uint64_t get_phys_address(uint64_t virt_addres);
+uint64_t get_phys_address(const uint64_t virt_addres);
 
 void memcpy(const void* src, void* dst, size_t size);
 void memset(void* dst, size_t size, uint8_t value);
+
+// Page X table entry
+typedef struct VMPxE {
+    uint64_t entry : 62;    // Physical base of the entry
+    uint64_t level : 2;     // Page table level from PML4(0) to PT(3)
+} ATTR_PACKED VMPxE;
+
+VMPxE get_pxe_of_virt_addr(const uint64_t address);
