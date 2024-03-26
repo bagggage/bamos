92fdda69b1f0f7c2f3b927d242d0e2f3ae44d191
bagggage <pigulevskiykostya@gmail.com>
2024-03-26 15:16:12 +0300

    CR2/CR3 Regs read

--- src/kernel/cpu/regs.h
+++ src/kernel/cpu/regs.h
@@ -13,3 +13,19 @@ static inline uint64_t cpu_get_rsp() {
 static inline void cpu_set_rsp(uint64_t rsp) {
     asm volatile("mov %0,%%rsp"::"a"(rsp));
 }
+
+static inline uint64_t cpu_get_cr2() {
+    uint64_t cr2;
+
+    asm volatile("mov %%cr2,%0":"=a"(cr2));
+
+    return cr2;
+}
+
+static inline uint64_t cpu_get_cr3() {
+    uint64_t cr3;
+
+    asm volatile("mov %%cr3,%0":"=a"(cr3));
+
+    return cr3;
+}
