aaaff64817d1bea5f3ee9681b736a8a834c8e6b4
bagggage <pigulevskiykostya@gmail.com>
2024-04-07 22:40:27 +0300

    128G -> 128M memory option

--- qemu.sh
+++ qemu.sh
@@ -7,7 +7,7 @@ export UEFI=${THIRD_PRT}/uefi/OVMF-efi.fd
 
 unset GTK_PATH
 qemu-img create -f raw ${DIST_DIR}/nvme.img 512K
-qemu-system-x86_64 -bios ${UEFI} -smp cores=8 -nic none -m 128G -no-reboot \
+qemu-system-x86_64 -bios ${UEFI} -smp cores=8 -nic none -m 128M -no-reboot \
 -drive file=${DIST_DIR}/bamos.iso,media=cdrom \
 -drive format=raw,file=${DIST_DIR}/nvme.img,if=none,id=nvme \
 -device nvme,drive=nvme,serial=1234
