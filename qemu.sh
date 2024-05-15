8a2dd5d23ad7ffbf72afe746190b87b00750577a
bagggage <pigulevskiykostya@gmail.com>
2024-05-15 22:26:10 +0300

    Added NVME Drive with ext2 fs

--- qemu.sh
+++ qemu.sh
@@ -6,9 +6,12 @@ set -e
 export UEFI=${THIRD_PRT}/uefi/OVMF-efi.fd
 
 unset GTK_PATH
-# qemu-img create -f raw ${DIST_DIR}/nvme.img 512K
+#qemu-img create -f raw ${DIST_DIR}/nvme.img 512M
+#mkfs -t ext2 ${DIST_DIR}/nvme.img
 qemu-system-x86_64 -enable-kvm -bios ${UEFI} -smp cores=8 -nic none -m 512M -no-reboot \
 -drive file=${DIST_DIR}/bamos.iso,media=cdrom \
+-blockdev node-name=nvm,driver=file,filename=${DIST_DIR}/nvme.img \
+-device nvme,id=nvme-ctrl-0,addr=06,serial=deadbeef,drive=nvm
 # -device nvme,id=nvme-ctrl-0,serial=deadbeef \
 # -drive file=${DIST_DIR}/nvme.img,if=none,id=nvm-1 \
 # -device nvme-ns,drive=nvm-1
