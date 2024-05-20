c1708189cc7b883749acd5d934867123ae67c344
GwinBest <thegwinbest@gmail.com>
2024-05-21 00:29:58 +0300

    added pow

--- src/kernel/math.c
+++ src/kernel/math.c
@@ -10,3 +10,17 @@ uint32_t log2(uint32_t number)
 
     return (popcount(number) - 1);
 }
+
+uint64_t pow(const uint64_t value, uint64_t power) {
+    if (power <= 1) return 1;
+
+    uint64_t return_value = value;
+
+    while (power > 1) {
+        return_value *= value;
+
+        --power;
+    }
+
+    return return_value;
+}
--- src/kernel/math.h
+++ src/kernel/math.h
@@ -35,3 +35,5 @@ uint32_t log2(uint32_t number);
 static inline uint32_t log2upper(uint32_t number) {
     return (popcount(number) > 1) ? (log2(number) + 1) : log2(number);
 }
+
+uint64_t pow(const uint64_t value, uint64_t power);
