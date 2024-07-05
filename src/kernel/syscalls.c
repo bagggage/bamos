dc4f1352232537ad1d5728f6fef305d6a2e25506
bagggage <pigulevskiykostya@gmail.com>
2024-07-05 10:57:45 +0300

    Implemented syscalls: `brk`, `openat`, `writev`. `mmap` in progress

--- src/kernel/syscalls.c
+++ src/kernel/syscalls.c
@@ -18,9 +18,12 @@
 #include "libc/unistd.h"
 #include "libc/sys/mman.h"
 #include "libc/sys/syscall.h"
+#include "libc/sys/uio.h"
 
 #include "proc/local.h"
 #include "proc/proc.h"
+
+#include "vm/buddy_page_alloc.h"
 
 #define ALIGN(x, a) (((x) + (a) - 1) & ~((a) - 1))
 
@@ -49,9 +52,18 @@ ATTR_NAKED void _syscall_handler() {
 
     {
         // Get syscall handler in asm, in case to use only rax register
+        asm volatile("push %rax");
         asm volatile("mov syscall_table(,%1,8),%0":"=r"(syscall):"r"(syscall));
 
-        if (syscall == 0) goto invalid_syscall;
+        if (syscall == 0) {
+            asm volatile("pop %rax");
+            store_syscall_frame();
+            invalid_syscall_msg(syscall);
+            restore_syscall_frame();
+            goto invalid_syscall;
+        }
+
+        asm volatile("add $8,%rsp");
 
         // Protect rcx,r11 from compiler allocation before saving
         USE(ip); USE(rflags);
@@ -194,24 +206,22 @@ long _sys_write(unsigned int fd, const char* buffer, size_t count) {
 }
 
 long _sys_open(const char* filename, int flags) {
-    ProcessorLocal* proc_local = proc_get_local();
-    //kernel_warn("SYS OPEN: CPU %x: %u: %x:%s, %u\n", proc_local, proc_local->idx, filename, filename, flags);
+    Process* const process = proc_get_local()->current_task->process;
+    kernel_warn("SYS OPEN: %x:%s, %u\n", filename, filename, flags);
 
     //if (filename[0] == '\0') raw_hexdump(filename, 16);
 
-    if (((flags & O_WRONLY) && (flags & O_RDWR)) ||
-        is_virt_addr_mapped_userspace(
-            proc_local->current_task->process->addr_space.page_table,
-            (const uint64_t)filename
-        ) == FALSE) {
-        return -EINVAL;
+    if (is_virt_addr_mapped_userspace(
+            process->addr_space.page_table,
+            (uint64_t)filename) == FALSE
+        )
+    {
+        return -EFAULT;
     }
 
-    long result = fd_open(
-        proc_local->current_task->process,
-        filename,
-        flags
-    );
+    if ((flags & O_WRONLY) && (flags & O_RDWR)) return -EINVAL;
+
+    long result = fd_open(process, NULL, filename, flags);
 
     //raw_hexdump((uint64_t)proc_local->instruction_ptr & (~0xFFull), 32);
     //kernel_warn("SYS OPEN: result: %i: ret: %x\n", result, proc_local->instruction_ptr);
@@ -225,58 +235,129 @@ long _sys_close(unsigned int fd) {
     return (fd_close(proc_local->current_task->process, fd) ? 0 : -EBADF);
 }
 
+static inline bool_t is_addr_in_range(const uint64_t address, const uint64_t length, const uint64_t base, const uint32_t pages_count) {
+    return address >= base && (address + length) <= (base + ((uint64_t)pages_count * PAGE_BYTE_SIZE));
+}
+
 long _sys_mmap(const void* address, size_t length, int protection, int flags, int fd, uint32_t offset) {
-    UNUSED(fd); UNUSED(offset);
+    Process* const process = proc_get_local()->current_task->process;
+    kernel_warn("SYS MMAP: %x; %x; %u; %u; %u; %u\n",
+        address, length, protection, flags, fd, offset);
 
-    ProcessorLocal* proc_local = proc_get_local();
-    //kernel_warn("SYS MMAP: CPU: %u: %x; %x; %u; %u; %u; %u\n", proc_local->idx,
-    //    address, length, protection, flags, fd, offset);
-
-    if (address != NULL || length == 0 ||
+    if (length == 0 ||
         protection == PROT_NONE ||
-        (protection & PROT_READ) == 0 ||
-        flags != (MAP_ANONYMOUS | MAP_PRIVATE)) {
+        (protection & (PROT_READ | PROT_EXEC)) == 0 ||
+        (flags & (MAP_ANONYMOUS | MAP_PRIVATE | MAP_FIXED) == 0)) {
         return -EINVAL;
     }
 
-    VMPageFrameNode* frame_node = proc_push_vm_page(proc_local->current_task->process);
-
-    if (frame_node == NULL) return -ENOMEM;
-
-    frame_node->frame = vm_alloc_pages(
-        div_with_roundup(length, PAGE_BYTE_SIZE),
-        &proc_local->current_task->process->addr_space.heap,
-        proc_local->current_task->process->addr_space.page_table,
-        (
-            VMMAP_USER_ACCESS |
-            ((protection & PROT_WRITE) ? VMMAP_WRITE : 0) |
-            ((protection & PROT_EXEC) ? VMMAP_EXEC : 0)
-        )
+    VMPageFrameNode* frame_node;
+    const uint32_t pages_count = div_with_roundup(length, PAGE_BYTE_SIZE);
+    const uint32_t map_flags = (
+        VMMAP_USER_ACCESS |
+        ((protection & PROT_WRITE) ? VMMAP_WRITE : 0) |
+        ((protection & PROT_EXEC) ? VMMAP_EXEC : 0)
     );
+    bool_t need_ctrl = FALSE;
 
-    if (frame_node->frame.count == 0) {
-        proc_dealloc_vm_page(proc_local->current_task->process, frame_node);
-        return -ENOMEM;
+    if (address == NULL) {
+        frame_node = proc_push_vm_page(process);
+        if (frame_node == NULL) return -ENOMEM;
+
+        if ((flags & MAP_ANONYMOUS) == 0 && (protection & PROT_WRITE) == 0) need_ctrl = TRUE;
+
+        frame_node->frame = vm_alloc_pages(
+            pages_count,
+            &process->addr_space.heap,
+            process->addr_space.page_table,
+            need_ctrl ? VMMAP_WRITE : map_flags
+        );
+
+        if (frame_node->frame.count == 0) {
+            proc_dealloc_vm_page(process, frame_node);
+            return -ENOMEM;
+        }
+
+        if (flags & MAP_ANONYMOUS) return (long)frame_node->frame.virt_address;
+    }
+    else {
+        if ((uint64_t)address + length > process->addr_space.heap.virt_top ||
+            (uint64_t)address < process->addr_space.heap.virt_base ||
+            is_virt_addr_range_mapped((uint64_t)address, div_with_roundup(length, PAGE_BYTE_SIZE)) == FALSE)
+            return -EINVAL;
+
+        frame_node = (void*)process->vm_pages.next;
+
+        do {
+            if (is_addr_in_range((uint64_t)address, length, frame_node->frame.virt_address, frame_node->frame.count)) {
+                break;
+            }
+
+            frame_node = frame_node->next;
+        } while (frame_node != NULL);
+
+        if (frame_node == NULL) return -EINVAL;
+
+        if (frame_node->frame.flags != map_flags) {
+            if ((map_flags & VMMAP_WRITE) != 0) {
+                vm_map_ctrl((uint64_t)address, process->addr_space.page_table, pages_count, map_flags);
+            }
+            else {
+                need_ctrl = TRUE;
+
+                if ((frame_node->frame.flags & VMMAP_WRITE) == 0) {
+                    vm_map_ctrl(address, process->addr_space.page_table, pages_count, VMMAP_WRITE);
+                }
+            }
+        }
+        else if ((frame_node->frame.flags & VMMAP_WRITE) == 0) {
+            need_ctrl = TRUE;
+            vm_map_ctrl(address, process->addr_space.page_table, pages_count, VMMAP_WRITE);
+        }
     }
 
-    return (long)frame_node->frame.virt_address;
+    long result = -EBADFD;
+    if (fd < 0 || fd >= process->files_capacity) goto out_fail;
+
+    const FileDescriptor* file = process->files[fd];
+    if (file == NULL || file->dentry->inode->type != VFS_TYPE_FILE) goto out_fail;
+
+    const uint64_t inner_offset = (address != NULL) ? address - frame_node->frame.virt_address : 0;
+    const uint64_t result_addr = frame_node->frame.virt_address + inner_offset;
+
+    vfs_read(file->dentry, offset, length, (void*)result_addr);
+
+    if (need_ctrl) {
+        vm_map_ctrl(result_addr, process->addr_space.page_table, pages_count, map_flags);
+    }
+
+    kernel_warn("MMAP: %x\n", result_addr);
+    return (long)result_addr;
+
+out_fail:
+    if (address == NULL) {
+        vm_free_pages(&frame_node->frame, &process->addr_space.heap, process->addr_space.page_table);
+        proc_dealloc_vm_page(process, frame_node);
+    }
+    kernel_warn("MMAP: %u\n", result);
+    return result;
 }
 
 long _sys_munmap(void* address, size_t length) {
     if (address == NULL || length == 0) return -EINVAL;
 
-    ProcessorLocal* proc_local = proc_get_local();
+    Process* const process = proc_get_local()->current_task->process;
 
-    spin_lock(&proc_local->current_task->process->vm_lock);
+    spin_lock(&process->vm_lock);
 
-    VMPageFrameNode* frame_node = (VMPageFrameNode*)proc_local->current_task->process->vm_pages.next;
+    VMPageFrameNode* frame_node = (VMPageFrameNode*)process->vm_pages.next;
 
     while (frame_node != NULL &&
         frame_node->frame.virt_address != (uint64_t)address) {
         frame_node = frame_node->next;
     }
 
-    spin_release(&proc_local->current_task->process->vm_lock);
+    spin_release(&process->vm_lock);
 
     if (frame_node == NULL) return -EINVAL;
     
@@ -284,33 +365,60 @@ long _sys_munmap(void* address, size_t length) {
 
     if (pages_count != frame_node->frame.count) return -EINVAL;
 
-    proc_dealloc_vm_page(proc_local->current_task->process, frame_node);
+    proc_dealloc_vm_page(process, frame_node);
 
     return 0;
 }
 
-long _sys_brk(uint64_t brk) {
-    UNUSED(brk);
+uint64_t _sys_brk(uint64_t brk) {
+    kernel_warn("SYS BRK: %x\n", brk);
 
-    return -ENOMEM;
+    Process* const process = proc_get_local()->current_task->process;
+
+    VMMemoryBlockNode* const last_seg = (void*)
+        process->addr_space.interp_seg == NULL ?
+        process->addr_space.segments.prev : process->addr_space.interp_seg->prev;
+
+    const uint64_t curr_brk = last_seg->block.virt_address + 
+        ((uint64_t)last_seg->block.pages_count * PAGE_BYTE_SIZE);
+
+    if (brk == 0) return curr_brk;
+
+    const int64_t diff = brk - curr_brk;
+
+    if (diff == 0) return brk;
+    if (diff < 0) return 0;
+
+    const uint32_t pages_count = div_with_roundup(diff, PAGE_BYTE_SIZE);
+    const uint32_t page_base = bpa_allocate_pages(log2upper(pages_count)) / PAGE_BYTE_SIZE;
+
+    if (page_base == 0) return -ENOMEM;
+
+    VMMemoryBlockNode* const new_brk_seg = proc_insert_segment(process, last_seg);
+
+    new_brk_seg->block.virt_address = curr_brk;
+    new_brk_seg->block.pages_count = pages_count;
+    new_brk_seg->block.page_base = page_base;
+
+    vm_map_phys_to_virt((uint64_t)page_base * PAGE_BYTE_SIZE, curr_brk, pages_count, VMMAP_USER_ACCESS | VMMAP_WRITE);
+
+    return curr_brk + ((uint64_t)pages_count * PAGE_BYTE_SIZE);
 }
 
 long _sys_pread64(unsigned int fd, char* buffer, size_t count, int64_t offset) {
-    ProcessorLocal* proc_local = proc_get_local();
+    Process* const process = proc_get_local()->current_task->process;
 
     if (count == 0) return -EINVAL;
     if (is_virt_addr_mapped_userspace(
-            proc_local->current_task->process->addr_space.page_table,
+            process->addr_space.page_table,
             (uint64_t)buffer
         ) == FALSE) {
         return -EFAULT;
     }
 
-    if (fd >= proc_local->current_task->process->files_capacity) {
-        return -EBADF;
-    }
+    if (fd >= process->files_capacity) return -EBADF;
 
-    FileDescriptor* file = proc_local->current_task->process->files[fd];
+    FileDescriptor* file = process->files[fd];
 
     if (file == NULL || (file->mode & O_WRONLY) != 0) return -EBADF;
     if (offset >= (int64_t)file->dentry->inode->file_size) return 0;
@@ -321,21 +429,19 @@ long _sys_pread64(unsigned int fd, char* buffer, size_t count, int64_t offset) {
 }
 
 long _sys_pwrite64(unsigned int fd, const char* buffer, size_t count, int64_t offset) {
-    ProcessorLocal* proc_local = proc_get_local();
+    Process* const process = proc_get_local()->current_task->process;
 
     if (count == 0) return -EINVAL;
     if (is_virt_addr_mapped_userspace(
-            proc_local->current_task->process->addr_space.page_table,
+           process->addr_space.page_table,
             (uint64_t)buffer
         ) == FALSE) {
         return -EFAULT;
     }
 
-    if (fd >= proc_local->current_task->process->files_capacity) {
-        return -EBADF;
-    }
+    if (fd >= process->files_capacity) return -EBADF;
 
-    FileDescriptor* file = proc_local->current_task->process->files[fd];
+    FileDescriptor* const file = process->files[fd];
 
     if (file == NULL ||
         (file->mode & O_WRONLY || file->mode & O_RDWR) == 0) {
@@ -345,6 +451,49 @@ long _sys_pwrite64(unsigned int fd, const char* buffer, size_t count, int64_t of
     const uint32_t writen = vfs_write(file->dentry, offset, count, (const void*)buffer);
 
     return writen;
+}
+
+long _sys_writev(int fd, const iovec* io_vec, int io_count) {
+    Process* const process = proc_get_local()->current_task->process;
+
+    if (io_count <= 0 || io_count > INT16_MAX) return -EINVAL;
+    if (io_vec == NULL ||
+        is_virt_addr_mapped_userspace(
+            process->addr_space.page_table, (uint64_t)io_vec
+        ) == FALSE) return -EFAULT;
+
+    if (fd >= process->files_capacity) return -EBADF;
+
+    FileDescriptor* const file = process->files[fd];
+
+    if (file == NULL ||
+        (file->mode & O_WRONLY || file->mode & O_RDWR) == 0) {
+        return -EBADF;
+    }
+
+    size_t total_size = 0;
+
+    for (int i = 0; i < io_count; ++i) {
+        if (is_virt_addr_mapped_userspace(
+            process->addr_space.page_table,
+            (uint64_t)io_vec[i].iov_base
+        ) == FALSE) return -EFAULT;
+
+        total_size += io_vec[i].iov_len;
+    }
+
+    if (total_size > INT64_MAX) return -EINVAL;
+
+    uint32_t total_writen = 0;
+
+    for (int i = 0; i < io_count; ++i) {
+        const uint32_t writen = vfs_write(file->dentry, file->cursor_offset, io_vec[i].iov_len, io_vec[i].iov_base);
+
+        file->cursor_offset += writen;
+        total_writen += writen;
+    }
+
+    return total_writen;
 }
 
 long _sys_access(const char* pathname, int mode) {
@@ -490,14 +639,13 @@ pid_t _sys_getppid() {
 }
 
 long _sys_arch_prctl(int code, const uint64_t address) {
-    const ProcessorLocal* proc_local = proc_get_local();
+    kernel_warn("SYS ARCH_PRCTL: CODE: %x\n", code);
 
+    const ProcessorLocal* proc_local = proc_get_local();
     const bool_t is_mapped = is_virt_addr_mapped_userspace(
         proc_local->current_task->process->addr_space.page_table,
         address
     );
-
-    kernel_msg("CODE: %x\n", code);
 
     bool_t is_get = FALSE;
     uint64_t value = 0;
@@ -531,6 +679,37 @@ long _sys_arch_prctl(int code, const uint64_t address) {
     return 0;
 }
 
+long _sys_openat(int dir_fd, const char* pathname, int flags, mode_t mode) {
+    kernel_warn("SYS OPENAT: %u: %s: %u: %u\n", dir_fd, pathname, flags, mode);
+
+    Process* const process = proc_get_local()->current_task->process;
+
+    const VfsDentry* dir_dentry;
+
+    if (dir_fd == AT_FDCWD) {
+        dir_dentry = process->work_dir;
+    }
+    else {
+        if (dir_fd >= process->files_capacity) return -EBADF;
+        const FileDescriptor* dir = process->files[dir_fd];
+
+        if (dir == NULL || dir->dentry->inode->type != VFS_TYPE_DIRECTORY) return -EBADF;
+        dir_dentry = dir->dentry;
+    }
+
+    if (is_virt_addr_mapped_userspace(
+            process->addr_space.page_table, 
+            (uint64_t)pathname
+        ) == FALSE)
+    {
+        return -EFAULT;
+    }
+
+    long result = fd_open(process, dir_dentry, pathname, flags);
+
+    return result;
+}
+
 void init_syscalls() {
     syscall_table[SYS_READ]     = (void*)&_sys_read;
     syscall_table[SYS_WRITE]    = (void*)&_sys_write;
@@ -545,6 +724,7 @@ void init_syscalls() {
     syscall_table[SYS_PREAD64]  = (void*)&_sys_pread64;
     syscall_table[SYS_PWRITE64] = (void*)&_sys_pwrite64;
 
+    syscall_table[SYS_WRITEV]   = (void*)&_sys_writev;
     syscall_table[SYS_ACCESS]   = (void*)&_sys_access;
 
     syscall_table[SYS_GETPID]   = (void*)&_sys_getpid;
@@ -563,4 +743,8 @@ void init_syscalls() {
     syscall_table[SYS_GETPPID]  = (void*)&_sys_getppid;
 
     syscall_table[SYS_ARCH_PRCTL] = (void*)&_sys_arch_prctl;
+
+    syscall_table[SYS_EXIT_GROUP] = (void*)&_sys_exit;
+
+    syscall_table[SYS_OPENAT]   = (void*)&_sys_openat;
 }
