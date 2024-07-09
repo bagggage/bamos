530b9e4a66115b780bc56fb9f8e51e919e80c60f
bagggage <pigulevskiykostya@gmail.com>
2024-07-09 21:44:45 +0300

    Boot alloc and memory map

--- src/kernel/boot.cpp
+++ src/kernel/boot.cpp
@@ -2,11 +2,15 @@
 
 #include <bootboot.h>
 
+#include "arch.h"
+#include "assert.h"
 #include "trace.h"
 
 #include "video/fb.h"
 
 extern BOOTBOOT bootboot;
+
+BootMemMap Boot::mem_map;
 
 ColorFormat bootboot_make_color_fmt(const uint8_t fb_type) {
     switch (fb_type)
@@ -50,6 +54,111 @@ const DebugSymbolTable* Boot::get_dbg_table() {
         }
 
         ptr++;
+    }
+
+    return nullptr;
+}
+
+uint32_t Boot::calc_mmap_size() {
+    return (
+        bootboot.size -
+        (reinterpret_cast<uint64_t>(&bootboot.mmap) -
+        reinterpret_cast<uint64_t>(&bootboot))
+    ) / sizeof(MMapEnt);
+}
+
+void* Boot::early_alloc(const uint32_t pages_num) {
+    const uint32_t size = calc_mmap_size();
+
+    MMapEnt* const entries = &bootboot.mmap;
+
+    for (uint32_t i = 0; i < size; ++i) {
+        if ((MMapEnt_Size(entries + i) / Arch::page_size) >= pages_num) {
+            const uintptr_t result = (
+                MMapEnt_Ptr(entries + i) + MMapEnt_Size(entries + i) -
+                (pages_num * Arch::page_size)
+            );
+
+            entries[i].size = (
+                (MMapEnt_Size(entries + i) - (pages_num * Arch::page_size)) |
+                MMapEnt_Type(entries + i)
+            );
+
+            return reinterpret_cast<void*>(result);
+        }
+    }
+
+    return nullptr;
+}
+
+void Boot::init_mem_map() {
+    mem_map.size = calc_mmap_size();
+    mem_map.entries = reinterpret_cast<BootMemMap::Entry*>(early_alloc(1));
+
+    uint32_t invalid_ents_num = 0;
+    uint32_t j = 0;
+
+    for (uint32_t i = 0; i < mem_map.size; ++i) {
+        const MMapEnt* boot_ent = &bootboot.mmap + i;
+        BootMemMap::Entry& ent = mem_map.entries[j];
+
+        if (MMapEnt_Size(boot_ent) == 0) [[unlikely]] continue;
+
+        if ((MMapEnt_Size(boot_ent) % Arch::page_size) > 0 ||
+            (MMapEnt_Ptr(boot_ent) % Arch::page_size) > 0
+        ) [[unlikely]] {
+            invalid_ents_num++;
+            continue;
+        }
+
+        ent.base = MMapEnt_Ptr(boot_ent) / Arch::page_size;
+        ent.pages = MMapEnt_Size(boot_ent) / Arch::page_size;
+
+        switch (MMapEnt_Type(boot_ent)) {
+        case MMAP_ACPI: [[fallthrough]];
+        case MMAP_MMIO: ent.type = BootMemMap::Type::MEM_DEV; break;
+        case MMAP_USED: ent.type = BootMemMap::Type::MEM_USED; break;
+        case MMAP_FREE: ent.type = BootMemMap::Type::MEM_FREE; break;
+        default: ent.type = BootMemMap::Type::MEM_USED; break;
+        }
+
+        j++;
+    }
+
+    mem_map.size = j;
+
+    if (invalid_ents_num) {
+        error("Invalid memory map entries: ", invalid_ents_num);
+    }
+}
+
+void BootMemMap::remove(const uint32_t idx) {
+    kassert(idx < size);
+
+    if (idx == size - 1) {
+        size--;
+        return;
+    }
+
+    for (uint32_t i = idx; i < size - 1; ++i) entries[i] = entries[i + 1];
+}
+
+void* Boot::alloc(const uint32_t pages_num) {
+    if (mem_map.is_empty()) init_mem_map();
+
+    for (uint32_t i = 0; i < mem_map.size; ++i) {
+        if (mem_map.entries[i].pages < pages_num) continue;
+
+        const uint64_t base = 
+            mem_map.entries[i].base +
+            mem_map.entries[i].pages -
+            pages_num;
+        
+        if (base == mem_map.entries[i].base) mem_map.remove(i);
+
+        mem_map.entries[i].pages -= pages_num;
+
+        return reinterpret_cast<void*>(base * Arch::page_size);
     }
 
     return nullptr;
--- src/kernel/boot.h
+++ src/kernel/boot.h
@@ -1,14 +1,54 @@
 #pragma once
 
 #include "definitions.h"
+#include "math.h"
 
 class Framebuffer;
 struct DebugSymbolTable;
 
+struct BootMemMap {
+    enum Type : uint8_t {
+        MEM_FREE,
+        MEM_DEV,
+        MEM_USED
+    };
+
+    struct Entry {
+        uint32_t base;
+        uint32_t pages;
+        Type type : 3;
+    };
+
+    Entry* entries = nullptr;
+    uint32_t size = 0;
+
+    inline bool is_empty() const { return size == 0; }
+
+    inline uint32_t get_max_page() const {
+        const Entry& ent = entries[size - 1];
+
+        return ent.base + ent.pages - 1;
+    }
+
+    void remove(const uint32_t idx);
+};
+
 class Boot {
+private:
+    static BootMemMap mem_map;
+
+    static uint32_t calc_mmap_size();
+    static void init_mem_map();
+
+    static void* early_alloc(const uint32_t size);
 public:
     static void get_fb(Framebuffer* const fb);
 
     static uint32_t get_cpus_num();
     static const DebugSymbolTable* get_dbg_table();
+    static inline BootMemMap& get_mem_map() {
+        return mem_map;
+    }
+
+    static void* alloc(const uint32_t pages_num);
 };
