3b4661fab4be02f0c3f559b34d6ec64560807751
bagggage <pigulevskiykostya@gmail.com>
2024-07-11 17:58:43 +0300

    Fixed some issues

--- src/kernel/math.h
+++ src/kernel/math.h
@@ -28,6 +28,8 @@ static inline uint32_t popcount(const uint32_t number) {
     return result;
 }
 
+uint32_t log2(uint32_t number);
+
 static inline uint32_t log2upper(uint32_t number) {
     return (popcount(number) > 1) ? (log2(number) + 1) : log2(number);
 }
--- src/kernel/utils/bitmap.h
+++ src/kernel/utils/bitmap.h
@@ -12,26 +12,44 @@ public:
     {}
 
     inline uint8_t get(const size_t bit_idx) {
-        const uint8_t bitmask = bit_idx % BYTE_SIZE;
+        const uint8_t bitmask = 1 << (bit_idx % BYTE_SIZE);
 
         return bytes[bit_idx / BYTE_SIZE] & bitmask;
     }
 
     inline void clear(const size_t bit_idx) {
-        const uint8_t bitmask = bit_idx % BYTE_SIZE;
+        const uint8_t bitmask = 1 << (bit_idx % BYTE_SIZE);
 
         bytes[bit_idx / BYTE_SIZE] &= ~bitmask;
     }
 
-    inline uint8_t set(const size_t bit_idx) {
-        const uint8_t bitmask = bit_idx % BYTE_SIZE;
+    inline void set(const size_t bit_idx) {
+        const uint8_t bitmask = 1 << (bit_idx % BYTE_SIZE);
 
         bytes[bit_idx / BYTE_SIZE] |= bitmask;
     }
 
-    inline uint8_t inverse(const size_t bit_idx) {
-        const uint8_t bitmask = bit_idx % BYTE_SIZE;
+    inline void inverse(const size_t bit_idx) {
+        const uint8_t bitmask = 1 << (bit_idx % BYTE_SIZE);
 
         bytes[bit_idx / BYTE_SIZE] ^= bitmask;
+    }
+
+    inline uint32_t find_clear() {
+        uint32_t bit_idx = 0;
+
+        while (bytes[bit_idx / BYTE_SIZE] == 0xFF) bit_idx += BYTE_SIZE;
+
+        const uint8_t byte = bytes[bit_idx / BYTE_SIZE];
+        const uint32_t end = bit_idx + BYTE_SIZE;
+        uint8_t bitmask = 1;
+
+        for (; bit_idx < end; ++bit_idx) {
+            if ((byte & bitmask) == 0) return bit_idx;
+
+            bitmask <<= 1;
+        }
+
+        return bit_idx;
     }
 };
