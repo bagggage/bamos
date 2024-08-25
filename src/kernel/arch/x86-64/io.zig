0654d54e0f845ce074b1735143be2be9d46f65d4
bagggage <pigulevskiykostya@gmail.com>
2024-08-25 21:55:22 +0300

    Small implementation of x86-64 I/O subsystem

---
+++ src/kernel/arch/x86-64/io.zig
@@ -0,0 +1,40 @@
+//! # Input-output subsystem implementation
+
+/// Write a byte into a I\O port.
+pub inline fn outb(byte: u8, port: u16) void {
+    asm volatile("outb %[d],%[p]"::[d]"{al}"(byte),[p]"{dx}"(port));
+}
+
+/// Write a word into a I\O port.
+pub inline fn outw(word: u16, port: u16) void {
+    asm volatile("outw %[d],%[p]"::[d]"{ax}"(word),[p]"{dx}"(port));
+}
+
+/// Write a double word into a I\O port.
+pub inline fn outl(dword: u32, port: u16) void {
+    asm volatile("outl %[d],%[p]"::[d]"{eax}"(dword),[p]"{dx}"(port));
+}
+
+/// Read a byte from I\O port.
+pub inline fn inb(port: u16) u8 {
+    var res: u8 = undefined;
+
+    asm("inb %[p],%[d]":[r]"={al}"(res):[p]"{dx}"(port));
+    return res;
+}
+
+/// Read a word from I\O port.
+pub inline fn inw(port: u32) u16 {
+    var res: u16 = undefined;
+
+    asm("inw %[p],%[d]":[r]"={ax}"(res):[p]"{dx}"(port));
+    return res;
+}
+
+/// Read a double word from I\O port.
+pub inline fn inl(port: u32) u32 {
+    var res: u32 = undefined;
+
+    asm("inl %[p],%[d]":[r]"={eax}"(res):[p]"{dx}"(port));
+    return res;
+}
