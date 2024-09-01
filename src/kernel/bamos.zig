de89c8bb37df9b138c388b3722b537e2a8b73dc0
bagggage <pigulevskiykostya@gmail.com>
2024-09-01 17:32:49 +0300

    Relative paths

--- src/kernel/bamos.zig
+++ src/kernel/bamos.zig
@@ -12,23 +12,23 @@
 //! ## Overview
 //! 
 //! The main kernel subsystems include:
-//! - [**Startup code**](/#bamos.start): The code for booting and initializing the kernel.
-//! - [**Boot module**](/#bamos.boot): A module that provides an abstraction
+//! - [**Startup code**](./#bamos.start): The code for booting and initializing the kernel.
+//! - [**Boot module**](./#bamos.boot): A module that provides an abstraction
 //! for interacting with data provided by the bootloader and the functionality
 //! necessary in the early stages of kernel initialization.
-//! - [**Virtual memory management module**](/#bamos.vm):
+//! - [**Virtual memory management module**](./#bamos.vm):
 //! This module organizes the management of all device memory,
 //! allowing for the dynamic allocation of physical memory pages
 //! and mapping them to virtual addresses through page tables.
 //! The module also includes various allocators and a general
 //! interface for architecture-dependent features.
-//! - [**Utilities**](/#bamos.utils): A set of auxiliary components,
+//! - [**Utilities**](./#bamos.utils): A set of auxiliary components,
 //! often various implementations of data structures,
 //! such as `utils.SList`, `utils.List`, `utils.BinaryTree`, `utils.Bitmap`, `utils.Heap`, and others.
-//! - [**Video module**](/#bamos.video):
+//! - [**Video module**](./#bamos.video):
 //! Contains code for interacting with the framebuffer and rendering text.
 //! **Note**: This module is temporary and will be removed after
 //! the implementation of a fully-fledged device management and driver system.
-//! - [**Architecture module**](/#bamos.arch.x86-64.arch): This module provides the implementation
+//! - [**Architecture module**](./#bamos.arch.x86-64.arch): This module provides the implementation
 //! of the interface for architecture-dependent features.
-//! - [**Logging**](/#bamos.log): The logging system.
+//! - [**Logging**](./#bamos.log): The logging system.
