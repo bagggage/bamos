816a576f98972c2247bee6cd4b96f2d2db91b11a
bagggage <pigulevskiykostya@gmail.com>
2024-12-05 03:25:58 +0300

    Redirect serial output to stdio

--- qemu.sh
+++ qemu.sh
@@ -14,8 +14,13 @@ fi
 unset GTK_PATH
 qemu-system-x86_64 \
  ${DBG} \
+ -chardev stdio,id=char0 \
+ -serial chardev:char0 \
  -bios ${UEFI} -nic none -no-reboot \
- -drive file=${DIST_DIR}/bamos.iso,media=cdrom \
+ -drive file=${DIST_DIR}/bamos.iso,format=raw,if=none,id=boot \
+ -drive file=/dev/nvme0n1,format=raw,if=none,id=img \
+ -device ide-hd,drive=boot,bootindex=0 \
+ -device nvme,serial=deadbeef,drive=img \
  -machine q35 -smp cores=4 -m 64M \
  -enable-kvm -cpu host \
 # ^^^^^^^^^^^^^^^^^^^^^^
