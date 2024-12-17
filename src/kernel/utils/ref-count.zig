bee0bf3009f360807e69c4ed54d4db0cbc7dba89
bagggage <pigulevskiykostya@gmail.com>
2024-12-18 01:37:48 +0300

    Improve `RefCounter`

--- src/kernel/utils/ref-count.zig
+++ src/kernel/utils/ref-count.zig
@@ -15,27 +15,56 @@ pub fn RefCount(comptime UintType: type) type {
     return struct {
         const Self = @This();
 
-        count: std.atomic.Value(UintType) = .{ .raw = 0 },
+        value: std.atomic.Value(UintType) = .{ .raw = 1 },
 
-        pub inline fn get(self: *Self) void {
-            // no synchronization necessary; just updating a counter.
-            _ = self.count.fetchAdd(1, .monotonic);
+        pub inline fn set(self: *Self, value: UintType) void {
+            self.value.store(value, .acquire);
         }
 
-        pub inline fn put(self: *Self) void {
+        pub inline fn get(self: *Self) bool {
+            var old = self.count();
+
+            while (true) {
+                if (old == 0) return false;
+
+                if (self.value.cmpxchgWeak(
+                    old, old + 1,
+                    .acquire, .monotonic)
+                ) |new_old| {
+                    old = new_old; continue;
+                }
+
+                return true;
+            }
+
+            unreachable;
+        }
+
+        pub inline fn put(self: *Self) bool {
             // release ensures code before put() happens-before the
             // count is decremented as dropFn could be called by then.
-            if (self.count.fetchSub(1, .release) == 1) {
+            if (self.value.fetchSub(1, .release) == 1) {
                 // seeing 1 in the counter means that other put()s have happened,
                 // but it doesn't mean that uses before each put() are visible.
-                // The load acquires the release-sequence created by previous unref()s
+                // The load acquires the release-sequence created by previous put()s
                 // in order to ensure visibility of uses before dropping.
-                _ = self.count.load(.acquire);
+                _ = self.value.load(.acquire);
+                return true;
             }
+
+            return false;
         }
 
         pub inline fn count(self: *const Self) UintType {
-            return self.count.load(.acquire);
+            return self.value.load(.acquire);
+        }
+
+        pub inline fn inc(self: *Self) void {
+            _ = self.value.fetchAdd(1, .monotonic);
+        }
+
+        pub inline fn dec(self: *Self) void {
+            _ = self.value.fetchSub(1, .release);
         }
     };
 }
