026af5026b64b0e0a5c674d6daede9570a959db5
bagggage <pigulevskiykostya@gmail.com>
2025-10-21 02:40:58 +0300

    vfs: File: better atomic access and mmap rework

--- src/kernel/vfs/File.zig
+++ src/kernel/vfs/File.zig
@@ -8,6 +8,7 @@ const File = @This();
 
 const Dentry = vfs.Dentry;
 const Error = vfs.Error;
+const sys = @import("../sys.zig");
 const utils = @import("../utils.zig");
 const vfs = @import("../vfs.zig");
 const vm = @import("../vm.zig");
@@ -15,7 +16,7 @@ const vm = @import("../vm.zig");
 pub const Operations = struct {
     pub const ReadFn = *const fn(*const Dentry, usize, []u8) Error!usize;
     pub const WriteFn = *const fn(*Dentry, usize, []const u8) Error!usize;
-    pub const MmapFn = *const fn(*const Dentry, usize, *const vm.VirtualRegion) Error!void;
+    pub const MmapFn = *const fn(*const Dentry, *sys.AddressSpace.MapUnit) Error!void;
     pub const IoctlFn = *const fn(*Dentry, c_uint, usize) Error!void;
 
     read: ReadFn,
@@ -32,6 +33,7 @@ pub const alloc_config: vm.obj.AllocatorConfig = .{
 
 dentry: *Dentry,
 ops: *const Operations = undefined,
+ref_count: utils.RefCount(u16) = .{},
 offset: usize = 0,
 
 pub inline fn init(self: *File, dentry: *Dentry) void {
@@ -48,6 +50,14 @@ pub inline fn assignDentry(self: *File, dentry: *Dentry) void {
     self.dentry = dentry;
 }
 
+pub inline fn get(self: *File) bool {
+    return self.ref_count.get();
+}
+
+pub inline fn put(self: *File) bool {
+    return self.ref_count.put();
+}
+
 pub inline fn releaseDentry(self: *File) void {
     self.dentry.deref();
 }
@@ -57,23 +67,33 @@ pub inline fn close(self: *File) void {
 }
 
 pub fn read(self: *File, buf: []u8) Error!usize {
-    const size = try self.ops.read(self.dentry, self.offset, buf);
-    self.offset += size;
+    const offset = self.offset;
+    const readed = try self.ops.read(self.dentry, offset, buf);
+    self.offset = offset + readed;
 
-    return size;
+    return readed;
+}
+
+pub fn readAll(self: *File, buf: []u8) Error!void {
+    const offset = self.offset;
+    const readed = try self.ops.read(self.dentry, offset, buf);
+    if (readed != buf.len) return Error.IoFailed;
+
+    self.offset = offset + readed;
 }
 
 pub inline fn write(self: *File, buf: []const u8) Error!usize {
     std.debug.assert(self.dentry.inode.type != .directory);
-    const size = try self.ops.write(self.dentry, self.offset, buf);
-    self.offset += size;
+    const offset = self.offset;
+    const size = try self.ops.write(self.dentry, offset, buf);
+    self.offset = offset + size;
 
     return size;
 }
 
-pub inline fn mmap(self: *File, offset: usize, region: *const vm.VirtualRegion) Error!void {
+pub inline fn mmap(self: *File, map_unit: *sys.AddressSpace.MapUnit) Error!void {
     std.debug.assert(self.dentry.inode.type != .directory);
-    return self.ops.mmap(self.dentry, offset, region);
+    return self.ops.mmap(self.dentry, map_unit);
 }
 
 pub inline fn ioctl(self: *File, cmd: c_uint, arg: usize) Error!void {
