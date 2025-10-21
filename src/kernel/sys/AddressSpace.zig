fd48b90c416657700a2d880b1bba99ee1e8aa544
bagggage <pigulevskiykostya@gmail.com>
2025-10-21 21:14:15 +0300

    sys: implement `AddressSpace` with `MapUnit`

---
+++ src/kernel/sys/AddressSpace.zig
@@ -0,0 +1,266 @@
+//! # Process Address Space
+
+// Copyright (C) 2025 Konstantin Pigulevskiy (bagggage@github)
+
+const std = @import("std");
+
+const utils = @import("../utils.zig");
+const vfs = @import("../vfs.zig");
+const vm = @import("../vm.zig");
+
+const VmList = MapUnit.List;
+const VmNode = MapUnit.Node;
+
+const RbTree = utils.rb.Tree(compareMapUnits);
+const RbNode = utils.rb.Node;
+
+const Self = @This();
+
+pub const MapUnit = @import("MapUnit.zig");
+
+pub const alloc_config: vm.obj.AllocatorConfig = .{
+    .allocator = .safe_oma,
+    .capacity = 128,
+    .wrapper = .none
+};
+
+page_table: *vm.PageTable,
+
+users: utils.RefCount(u32) = .init(0),
+
+map_units: VmList = .{},
+map_lock: utils.RwLock = .{},
+rb_tree: RbTree = .{},
+
+/// The last map unit before heap first free region starts.
+heap_unit: ?*MapUnit = null,
+
+/// Stack size in pages.
+stack_pages: u16,
+
+pub fn init(self: *Self, pt: *vm.PageTable, stack_pages: u16) void {
+    self.* = .{
+        .page_table = pt,
+        .stack_pages = stack_pages
+    };
+}
+
+pub fn create(stack_pages: u16) !*Self {
+    const self = vm.obj.new(Self) orelse return error.NoMemory;
+    errdefer vm.obj.free(Self, self);
+
+    const pt = vm.newPt() orelse return error.NoMemory;
+    self.init(pt, stack_pages);
+
+    return self;
+}
+
+pub fn deinit(self: *Self) void {
+    while (self.map_units.popFirst()) |n| {
+        n.data.deinit();
+        vm.obj.free(MapUnit, &n.data);
+    }
+
+    vm.freePt(self.page_table);
+}
+
+pub inline fn delete(self: *Self) void {
+    self.deinit();
+    vm.obj.free(Self, self);
+}
+
+pub inline fn ref(self: *Self) void {
+    self.users.inc();
+}
+
+pub inline fn deref(self: *Self) void {
+    if (self.users.put()) self.delete();
+}
+
+pub fn heapAllocRegion(self: *Self, pages: u32) ?usize {
+    const size = pages * vm.page_size;
+
+    var base_unit = self.heap_unit orelse return 0;
+    var free_base = base_unit.top();
+
+    if (free_base + size > vm.max_user_heap_addr) return null;
+
+    var next_node = base_unit.rb_node.next();
+    while (next_node) |n| : (next_node = n.next()) {
+        const next_unit = MapUnit.fromRbNode(n);
+        const free_size = next_unit.base() - free_base;
+
+        if (free_size >= size) return free_base;
+
+        base_unit = next_unit;
+        free_base = base_unit.top();
+
+        if (free_base + size > vm.max_user_heap_addr) return null;
+    }
+
+    return free_base;
+}
+
+pub fn map(self: *Self, map_unit: *MapUnit) !void {
+    // Handle collisions.
+    while (self.rb_tree.insert(&map_unit.rb_node)) |node| {
+        const col_unit = MapUnit.fromRbNode(node);
+
+        const map_base = map_unit.base();
+        const map_top = map_unit.top();
+        const col_top = col_unit.top();
+
+        if (map_base > col_unit.base()) {
+            if (map_top >= col_top) {
+                // Shrink from the top.
+                const pages = (col_top - map_base) / vm.page_size;
+                try col_unit.shrinkTop(@truncate(pages), self.page_table);
+
+                continue;
+            }
+
+            // Smaller than collided unit - divide.
+            try self.divideMapping(col_unit, map_unit);
+        } else if (map_top > col_top) {
+            // Bigger then collided unit - delete.
+            self.deleteMapping(col_unit);
+        } else if (map_top < col_top) {
+            // Shrink from the bottom.
+            const pages = (map_top - col_unit.base()) / vm.page_size;
+            try col_unit.shrinkBottom(@truncate(pages), self.page_table);
+        } else {
+            // Equals.
+            self.replaceMapping(col_unit, map_unit);
+            break;
+        }
+    }
+
+    self.map_units.prepend(map_unit.asNode());
+}
+
+pub fn pageFault(self: *Self, addr: usize, cause: vm.FaultCause) !void {
+    // Page aligned base address.
+    const base = addr - (addr % vm.page_size);
+    const top = base + vm.page_size;
+
+    const map_unit = blk: {
+        self.map_lock.readLock();
+        defer self.map_lock.readUnlock();
+
+        break :blk self.lookupMapUnit(
+            addr, base, top
+        ) orelse return error.NoEnt;
+    };
+
+    try map_unit.ops.pageFault(map_unit, addr, cause);
+}
+
+pub fn format(
+    self: *const Self,
+    comptime _: []const u8,
+    _: std.fmt.FormatOptions,
+    writer: anytype,
+) !void {
+    const stack_size = self.stack_pages * (vm.page_size / utils.kb_size);
+
+    try writer.print("{*}: refs: {}\n\t{*}, stack size: {} KB\n", .{
+        self, self.users.count(), self.page_table, stack_size
+    });
+
+    var node = self.rb_tree.first();
+    while (node) |n| : (node = n.next()) {
+        const map_unit = MapUnit.fromRbNode(n);
+
+        try writer.writeByte('\t');
+        try map_unit.format(&.{}, .{}, writer);
+        try writer.writeByte('\n');
+    }
+}
+
+fn lookupMapUnit(self: *Self, base: usize, top: usize) ?*MapUnit {
+    const rb_node = self.rb_tree.root;
+
+    while (rb_node) |n| {
+        const map_unit = MapUnit.fromRbNode(n);
+        const order = compareMapRegions(
+            base, top,
+            map_unit.base(), map_unit.top()
+        );
+
+        switch (order) {
+            .lt => rb_node = n.left,
+            .gt => rb_node = n.right,
+            .eq => return map_unit
+        }
+    }
+
+    return null;
+}
+
+fn compareMapUnits(left: *RbNode, right: *RbNode, _: ?*RbNode) std.math.Order {
+    const lhs_mapping = MapUnit.fromRbNode(left);
+    const rhs_mapping = MapUnit.fromRbNode(right);
+
+    return compareMapRegions(
+        lhs_mapping.base(), lhs_mapping.top(),
+        rhs_mapping.base(), rhs_mapping.top()
+    );
+}
+
+inline fn compareMapRegions(
+    lhs_base: usize, lhs_top: usize,
+    rhs_base: usize, rhs_top: usize
+) std.math.Order {
+    if (lhs_base >= rhs_top) {
+        return .gt;
+    } else if (rhs_base >= lhs_top) {
+        return .lt;
+    }
+
+    return .eq;
+}
+
+fn divideMapping(self: *Self, map_unit: *MapUnit, div_unit: *MapUnit) !void {
+    const new_base = div_unit.top();
+    const new_gap: u32 = @truncate((new_base - map_unit.base()) / vm.page_size);
+    const new_pg_size: u32 = @truncate((map_unit.top() - new_base) / vm.page_size);
+    const new_pg_off: u32 = new_gap + map_unit.page_offset;
+
+    const new_unit = vm.obj.new(MapUnit) orelse return error.NoMemory;
+    errdefer vm.obj.free(MapUnit, new_unit);
+
+    const map_pg_size = (div_unit.base() - map_unit.base()) / vm.page_size;
+    try map_unit.unmapRegion(
+        @truncate(map_pg_size),
+        div_unit.page_capacity, self.page_table
+    );
+
+    new_unit.init(
+        map_unit.file, new_base, new_pg_off,
+        new_pg_size, undefined
+    );
+    new_unit.flags = map_unit.flags;
+
+    const pg_off = map_unit.page_capacity - new_pg_size;
+    try map_unit.reinsertRegion(new_unit, pg_off, new_pg_size);
+
+    self.map_units.prepend(new_unit.asNode());
+    _ = self.rb_tree.insert(&new_unit.rb_node);
+}
+
+fn replaceMapping(self: *Self, old: *MapUnit, new: *MapUnit) void {
+    self.rb_tree.replace(&old.rb_node, &new.rb_node);
+
+    self.map_units.remove(old.asNode());
+    old.unmap(self.page_table);
+
+    vm.obj.delete(MapUnit, old);
+}
+
+fn deleteMapping(self: *Self, map_unit: *MapUnit) void {
+    self.rb_tree.remove(&map_unit.rb_node);
+    self.map_units.remove(map_unit.asNode());
+    map_unit.unmap(self.page_table);
+
+    vm.obj.delete(MapUnit, map_unit);
+}
---
+++ src/kernel/sys/MapUnit.zig
@@ -0,0 +1,420 @@
+//! # Mapping Unit
+
+// Copyright (C) 2025 Konstantin Pigulevskiy (bagggage@github)
+
+const std = @import("std");
+
+const rb = utils.rb;
+const utils = @import("../utils.zig");
+const vfs = @import("../vfs.zig");
+const vm = @import("../vm.zig");
+
+const Self = @This();
+
+pub const List = utils.SList(Self);
+pub const Node = List.Node;
+
+pub const Flags = packed struct {
+    map: vm.MapFlags = .{},
+
+    shared: bool = false,
+    grow_down: bool = false,
+
+    comptime { std.debug.assert(@sizeOf(Flags) == 2); }
+};
+
+pub const Operations = struct {
+    pub const PageFaultFn = *const fn(*Self, usize, vm.FaultCause) vfs.Error!void;
+
+    pageFault: PageFaultFn = &defaultPageFault,
+
+    fn defaultPageFault(self: *Self, addr: usize, cause: vm.FaultCause) vfs.Error!void {
+        if (!self.isAnonymous() or
+            self.flags.map.none or
+            (cause == .exec and !self.flags.map.exec) or
+            (cause == .write and !self.flags.map.write)
+        ) return vfs.Error.SegFault;
+
+        const page_offset: u32 = @truncate((addr - self.base()) / vm.page_size);
+        try self.region.insertNewPage(page_offset, 0, self.flags.map);
+    }
+};
+
+pub const Page = vm.VirtualRegion.Page;
+
+pub const PageHandle = struct {
+    prev: ?*Page.Node,
+    page: *Page,
+};
+
+pub const alloc_config: vm.obj.AllocatorConfig = .{
+    .allocator = .safe_oma,
+    .capacity = 256,
+    .wrapper = .listNode(Node)
+};
+
+pub const max_pages = Page.max_index + vm.PageAllocator.max_alloc_pages;
+pub const max_size = max_pages * vm.page_size;
+
+pub const default_ops: Operations = .{};
+
+/// File pointer, if file is mapped.
+file: ?*vfs.File = null,
+
+region: vm.VirtualRegion,
+/// File inner offset in pages.
+page_offset: u32,
+/// Virtual size of the mapping in pages.
+page_capacity: u32,
+
+ops: *const Operations = &default_ops,
+flags: Flags,
+
+rb_node: rb.Node = .{},
+
+pub fn init(
+    self: *Self, file: ?*vfs.File, virt: usize,
+    page_offset: u32, pages: u32, map_flags: vm.MapFlags
+) void {
+    if (file) |f| f.ref();
+    self.* = .{
+        .file = file,
+        .page_offset = page_offset,
+        .page_capacity = pages,
+        .region = .{ .base = virt },
+        .flags = .{ .map = map_flags },
+    };
+}
+
+pub inline fn deinit(self: *Self) void {
+    if (self.file) |f| f.deref();
+    self.region.deinit(self.isAnonymous());
+}
+
+pub inline fn asNode(self: *Self) *Node {
+    return @fieldParentPtr("data", self);
+}
+
+pub inline fn fromRbNode(node: *rb.Node) *Self {
+    return @fieldParentPtr("rb_node", node);
+}
+
+/// Returns virtual top of the mapping.
+pub inline fn top(self: *const Self) usize {
+    return self.region.base + self.size();
+}
+
+/// Returns virtual base of the mapping.
+pub inline fn base(self: *const Self) usize {
+    return self.region.base;
+}
+
+/// Returns virtual size of the mapping.
+pub inline fn size(self: *const Self) usize {
+    return @as(usize, self.page_capacity) * vm.page_size;
+}
+
+pub inline fn isAnonymous(self: *const Self) bool {
+    return self.file == null;
+}
+
+pub fn unmap(self: *Self, pt: *vm.PageTable) void {
+    self.region.unmap(pt, self.isAnonymous());
+}
+
+pub fn unmapRegion(self: *Self, page_offset: u32, pages: u32, pt: *vm.PageTable) !void {
+    const detached = try self.detachPages(page_offset, pages);
+    var node = detached.first;
+
+    if (self.isAnonymous()) {
+        while (node) |n| {
+            n.data.unmap(self.base(), pt);
+            vm.PageAllocator.free(n.data.getPhysBase(), n.data.rank);
+
+            node = n.next;
+            vm.obj.free(Page, &n.data);
+        }
+    } else {
+        while (node) |n| {
+            n.data.unmap(self.base(), pt);
+
+            node = n.next;
+            vm.obj.free(Page, &n.data);
+        }
+    }
+}
+
+pub fn shrinkTop(self: *Self, pages: u32, pt: *vm.PageTable) !void {
+    std.debug.assert(self.page_capacity > pages);
+
+    const idx = self.page_capacity - pages;
+    try self.unmapRegion(idx, pages, pt);
+
+    self.page_capacity -= pages;
+}
+
+pub fn shrinkBottom(self: *Self, pages: u32, pt: *vm.PageTable) !void {
+    std.debug.assert(self.page_capacity > pages);
+
+    try self.unmapRegion(0, pages, pt);
+
+    self.region.base += pages * vm.page_size;
+    self.page_capacity -= pages;
+}
+
+pub inline fn reinsertRegion(self: *Self, target: *Self, page_offset: u32, pages: u32) !void {
+    try self.detachPagesTo(
+        page_offset, pages,
+        &target.region.page_list
+    );
+}
+
+pub fn format(
+    self: *const Self,
+    comptime _: []const u8,
+    _: std.fmt.FormatOptions,
+    writer: anytype,
+) !void {
+    const map_flags = self.flags.map;
+    const flags_str: [4]u8 = .{
+        if (map_flags.none) '-' else 'r',
+        if (map_flags.write) 'w' else '-',
+        if (map_flags.exec) 'x' else '-',
+        if (self.isAnonymous()) 'a' else 'p'
+    };
+
+    try writer.print("{x:0>8} - {x:0>8}: {s}: {x:0>8} ", .{
+        self.base(), self.base() + self.size(),
+        &flags_str, @as(usize, self.page_offset),
+    });
+
+    if (self.file) |f| {
+        try f.dentry.path().format(&.{}, .{}, writer);
+    }
+}
+
+/// Detach mapped pages from the
+/// specified region and return a list of them.
+inline fn detachPages(self: *Self, page_offset: u32, pages: u32) !Page.List {
+    var list: Page.List = .{};
+    try detachPagesTo(self, page_offset, pages, &list);
+
+    return list;
+}
+
+/// Detach mapped pages from the
+/// specified region to the list.
+fn detachPagesTo(self: *Self, page_base: u32, page_len: u32, list: *Page.List) !void {
+    const page_top = page_base + page_len;
+
+    while (self.findPage(page_base, page_len)) |h| {
+        const col_base = h.page.base;
+        const col_len = h.page.pagesNum();
+        const col_top = h.page.idx + col_len;
+
+        // Shrink or divide this physical region if needed.
+        if (page_base > col_base) {
+            if (page_top >= col_top) {
+                // Shrink from the top.
+                try shrinkPageTop(list, h.page, page_base);
+            } else {
+                // Smaller than collided page - divide.
+                try dividePages(list, h.page, page_base, page_top);
+                break;
+            }
+        } else if (page_top < col_top) {
+            // Shrink from the bottom.
+            try shrinkPageBottom(list, h.page, page_top);
+        } else {
+            self.detachPage(list, &h);
+        }
+    }
+}
+
+fn detachPage(self: *Self, list: *Page.List, handle: *const PageHandle) void {
+    const node = handle.page.asNode();
+
+    // Remove page from the list.
+    if (handle.prev) |p| {
+        p.next = node.next;
+    } else {
+        self.region.page_list.first = node.next;
+    }
+
+    list.prepend(node);
+}
+
+fn findPage(self: *Self, page_base: u32, page_len: u32) ?PageHandle {
+    const page_top = page_base + page_len;
+
+    var prev: ?*Page.Node = null;
+    var node: ?*Page.Node = self.region.page_list.first;
+
+    while (node) |n| {
+        const n_base: u32 = n.data.idx;
+        const n_len: u32 = n.data.pagesNum();
+        const n_top: u32 = n_base + n_len;
+
+        if (page_base < n_top and n_base < page_top) {
+            return .{
+                .prev = prev,
+                .page = &n.data,
+            };
+        }
+
+        prev = node;
+        node = n.next;
+    }
+
+    return null;
+}
+
+fn shrinkPageTop(list: *Page.List, page: *Page, new_top_idx: u32) !void {
+    const page_len = page.pagesNum();
+    const page_top_idx = page_len + page.idx;
+    const detach_len = page_top_idx - new_top_idx;
+    const page_new_len = page_len - detach_len;
+    const detach_base = page.base + page_new_len;
+
+    const node_dump = page.asNode().*;
+    const list_end = list.first;
+
+    // Free new pages on error.
+    var dummy_list: Page.List = .{ .first = page.asNode() };
+
+    try buildPage( // Detached pages
+        list, detach_base,
+        new_top_idx, detach_len, false
+    );
+
+    errdefer freePageList(list, list_end);
+    errdefer page.asNode().* = node_dump;
+
+    try buildPage( // Rebuilded pages
+        &dummy_list, page.base,
+        page.idx, page_len, true
+    );
+}
+
+fn shrinkPageBottom(list: *Page.List, page: *Page, new_idx: u32) !void {
+    const detach_len = new_idx - page.idx;
+    const page_new_base = page.base + detach_len;
+    const page_new_len = page.pagesNum() - detach_len;
+
+    const node_dump = page.asNode().*;
+    const list_end = list.first;
+
+    var dummy_list: Page.List = .{ .first = page.asNode() };
+
+    try buildPage( // Detached pages.
+        list, page.base,
+        page.idx, detach_len, false
+    );
+
+    errdefer freePageList(list, list_end);
+    errdefer page.asNode().* = node_dump;
+
+    try buildPage( // Rebuilded pages.
+        &dummy_list, page_new_base,
+        new_idx, page_new_len, true
+    );
+}
+
+fn dividePages(list: *Page.List, page: *Page, div_idx: u32, div_top_idx: u32) !void {
+    const page_new_len = div_idx - page.idx;
+    const div_len = div_top_idx - div_idx;
+    const div_base = page.base + page_new_len;
+    const page_next_len = page.pagesNum() - page_new_len - div_len;
+    const page_next_base = page.base + page_new_len + div_len;
+
+    const node_dump = page.asNode().*;
+    const dummy_end = page.asNode().next;
+    var dummy_list: Page.List = .{ .first = page.asNode() };
+
+    errdefer page.asNode().* = node_dump;
+
+    try buildPage( // Build left part of the page.
+        &dummy_list, page.base,
+        page.idx, page_new_len, true
+    );
+    errdefer {
+        var clean_list: Page.List = .{ .first = page.asNode().next };
+        freePageList(&clean_list, dummy_end);
+    }
+
+    try buildPage( // Build right part of the page.
+        &dummy_list, page_next_base,
+        div_top_idx, page_next_len, false
+    );
+
+    try buildPage( // Build detached part.
+        list, div_base,
+        div_idx, div_len, false
+    );
+}
+
+fn buildPage(
+    list: *Page.List, phys_base: u32,
+    idx: u32, len: u32, comptime reuse_first: bool
+) !void {
+    var temp_base = phys_base;
+    var temp_idx = idx;
+    var temp_len = len;
+
+    const insert_after = reuse_first or (list.first != null);
+    const list_end = if (insert_after) list.first.?.next else list.first;
+
+    errdefer if (insert_after) {
+        var dummy_list: Page.List = .{ .first = list.first.?.next };
+        defer list.first.?.next = list_end;
+
+        freePageList(&dummy_list, list_end);
+    } else {
+        freePageList(list, list_end);
+    };
+
+    while (temp_len > 0) {
+        const new_page =
+            if (reuse_first and temp_base == phys_base)
+                &list.first.?.data
+            else
+                (vm.obj.new(Page) orelse return error.NoMemory);
+
+        var temp_rank: u8 = std.math.log2_int(u32, temp_len);
+        var rank_pages_num: u32 = @as(u32, 1) << @truncate(temp_rank);
+
+        while (utils.modByPowerOfTwo(u32, rank_pages_num, @truncate(temp_rank)) != 0) {
+            temp_rank -= 1;
+            rank_pages_num >>= 1;
+        }
+
+        new_page.* = .{
+            .base = temp_base,
+            .idx = @truncate(temp_idx),
+            .rank = temp_rank
+        };
+
+        // Insert new page node after current page.
+        if ((comptime !reuse_first) or temp_base != phys_base) {
+            const new_node = new_page.asNode();
+
+            if (insert_after)
+                list.first.?.insertAfter(new_node)
+            else
+                list.prepend(new_node);
+        }
+
+        temp_base += rank_pages_num;
+        temp_idx += rank_pages_num;
+        temp_len -= rank_pages_num;
+    }
+}
+
+fn freePageList(list: *Page.List, end: ?*Page.Node) void {
+    while (list.first) |n| {
+        if (n == end) break;
+
+        list.first = n.next;
+        vm.obj.free(Page, &n.data);
+    }
+}
