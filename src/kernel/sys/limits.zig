6ba4dd89f204d45c598268163a7c5d22f7602d51
bagggage <pigulevskiykostya@gmail.com>
2025-10-31 23:35:59 +0300

    sys: add limits defenitions for userspace related things

---
+++ src/kernel/sys/limits.zig
@@ -0,0 +1,15 @@
+//! # System limits, default values, and constants 
+
+// Copyright (C) 2025 Konstantin Pigulevskiy (bagggage@github)
+
+const std = @import("std");
+
+const utils = @import("../utils.zig");
+
+pub const default_stack_size = 2 * utils.mb_size;
+pub const default_max_open_files = 1024;
+pub const default_max_threads = 8192;
+pub const default_max_process = 65565;
+
+pub var max_threads = default_max_threads;
+pub var max_process = default_max_process;
