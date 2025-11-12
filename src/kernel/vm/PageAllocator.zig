38e00e836978e9029752571a79b1d842cbf688da
bagggage <pigulevskiykostya@gmail.com>
2025-11-12 21:59:32 +0300

    vm: PageAllocator: refactored - rank now is type of `u8`; - removed `getNode`, `makeNode` renamed to `nodeFromPage`; - other code-style improvements.

--- src/kernel/vm/PageAllocator.zig
+++ src/kernel/vm/PageAllocator.zig
@@ -3,7 +3,7 @@
 //! Provides functions for allocating and freeing pages, 
 //! and accessing the status of the free/used physical memory.
 
-// Copyright (C) 2024 Konstantin Pigulevskiy (bagggage@github)
+// Copyright (C) 2024-2025 Konstantin Pigulevskiy (bagggage@github)
 
 const std = @import("std");
 
@@ -15,7 +15,6 @@ const log = std.log.scoped(.PageAllocator);
 
 const List = utils.SList;
 const Node = List.Node;
-const Spinlock = utils.Spinlock;
 
 /// Represents a free memory area in the buddy allocator. 
 /// It maintains a list of free nodes and a bitmap for tracking free pages.
@@ -27,9 +26,9 @@ const FreeArea = struct {
     /// There are two states: allocated and free.
     /// But for optimization purposes state not stored directly within the bitmap.
     /// Each bit represents the difference of states between two neighbour pages.
-    bitmap: utils.Bitmap = undefined,
+    bitmap: utils.Bitmap = .{},
 
-    pub fn format(value: @This(), writer: *std.Io.Writer) std.Io.Writer.Error!void {
+    pub fn format(value: *FreeArea, writer: *std.Io.Writer) std.Io.Writer.Error!void {
         try writer.print("Free list: ", .{});
 
         var curr_node = value.list.first;
@@ -41,7 +40,7 @@ const FreeArea = struct {
         try writer.print("{{ ", .{});
     
         while (curr_node) |node| : (curr_node = node.next) {
-            try writer.print("0x{x}", .{entGetPhys(node)});
+            try writer.print("0x{x}", .{getPageNodePhys(node)});
 
             if (node.next != null) try writer.print(", ", .{});
         }
@@ -59,7 +58,7 @@ export var allocated_pages: u32 = 0;
 export var total_pages: usize = 0;
 
 var free_areas: [max_areas]FreeArea = .{ FreeArea{} } ** max_areas;
-var lock = Spinlock.init(.unlocked);
+var lock: utils.Spinlock = .init(.unlocked);
 
 var is_initialized = false;
 
@@ -77,20 +76,16 @@ pub fn init() vm.Error!void {
     const mem_pool = boot.alloc(bitmap_pages) orelse return vm.Error.NoMemory;
     const virt_pool = vm.getVirtLma(mem_pool);
 
-    log.warn("mem pool size: {} bytes ~ {} KB", .{bitmap_size, @as(usize, bitmap_pages) * (vm.page_size / utils.kb_size)});
-
+    log.warn("mem pool size: {} bytes ~ {} KiB", .{bitmap_size, @as(usize, bitmap_pages) * (vm.page_size / utils.kb_size)});
     initAreas(virt_pool, bitmap_size);
 
     allocated_pages += bitmap_pages;
-    allocated_pages += @truncate(
-        (@intFromPtr(vm.kernel_end) - @intFromPtr(vm.kernel_start)) /
-        vm.page_size
-    );
+    allocated_pages += @intCast((@intFromPtr(vm.kernel_end) - @intFromPtr(vm.kernel_start)) / vm.page_size);
     total_pages += allocated_pages;
 
     {
         const total_kb = total_pages * vm.page_size / utils.kb_size;
-        log.warn("total mem: {} KB ({} MB)", .{ total_kb, total_kb / utils.kb_size });
+        log.warn("total mem: {} KiB ({} MiB)", .{ total_kb, total_kb / utils.kb_size });
     }
 
     is_initialized = true;
@@ -100,58 +95,54 @@ pub fn init() vm.Error!void {
 /// 
 /// - `rank`: Determines the number of pages as `2^rank`.
 /// - Returns: The physical address of the allocated pages, or `null` if allocation fails.
-pub fn alloc(rank: u32) ?usize {
-    std.debug.assert(rank < max_rank);
+pub fn alloc(rank: u8) ?usize {
+    if (rank >= max_rank) return null;
 
     lock.lock();
     defer lock.unlock();
 
-    var free_entry = free_areas[rank].list.popFirst();
-    if (free_entry == null) {
-        var temp_rank = rank + 1;
-
-        while (temp_rank < max_areas) : (temp_rank += 1) {
-            if (free_areas[temp_rank].list.first) |entry| {
-                free_entry = entry;
-                break;
-            }
-        }
-
-        const entry = free_entry orelse return null;
-
-        var temp_pages: u32 = @as(u32, 1) << @truncate((temp_rank - 1));
-        var temp_base: u32 = entGetBase(entry);
-
-        const node = free_areas[temp_rank].list.popFirst().?;
-        free_areas[temp_rank - 1].list.prepend(node);
-
-        clearPageBit(temp_base, temp_rank);
-        setPageBit(temp_base, temp_rank - 1);
-
-        temp_rank -= 1;
-        temp_base += temp_pages;
-
-        while (temp_rank > rank) {
-            temp_rank -= 1;
-            temp_pages >>= 1;
-
-            const new_node = makeNode(temp_base);
-
-            free_areas[temp_rank].list.prepend(new_node);
-            setPageBit(temp_base, temp_rank);
-
-            temp_base += temp_pages;
-        }
+    if (free_areas[rank].list.popFirst()) |page_node| {
+        togglePageBit(getPageNodeBase(page_node), rank);
 
         allocated_pages += @as(u32, 1) << @truncate(rank);
-        return @as(usize, temp_base) * vm.page_size;
+        return getPageNodePhys(page_node);
     }
 
-    const entry = free_entry.?;
-    togglePageBit(entGetBase(entry), rank);
+    var temp_rank = rank + 1;
+    const entry = blk: {
+        while (temp_rank < max_areas) : (temp_rank += 1) {
+            if (free_areas[temp_rank].list.popFirst()) |entry| {
+                break :blk entry;
+            }
+        }
+        return null;
+    };
+
+    var temp_pages: u32 = @as(u32, 1) << @truncate((temp_rank - 1));
+    var temp_base: u32 = getPageNodeBase(entry);
+
+    free_areas[temp_rank - 1].list.prepend(entry);
+
+    clearPageBit(temp_base, temp_rank);
+    setPageBit(temp_base, temp_rank - 1);
+
+    temp_rank -= 1;
+    temp_base += temp_pages;
+
+    while (temp_rank > rank) {
+        temp_rank -= 1;
+        temp_pages >>= 1;
+
+        const new_node = nodeFromPage(temp_base);
+
+        free_areas[temp_rank].list.prepend(new_node);
+        setPageBit(temp_base, temp_rank);
+
+        temp_base += temp_pages;
+    }
 
     allocated_pages += @as(u32, 1) << @truncate(rank);
-    return entGetPhys(entry);
+    return @as(usize, temp_base) * vm.page_size;
 }
 
 /// Frees a physical memory of the specified rank (size).
@@ -159,7 +150,7 @@ pub fn alloc(rank: u32) ?usize {
 /// - `base`: Physical address of the first page of a linear block returned from `alloc`.
 /// - `rank`: Determines the number of pages as `2^rank`,
 /// must be the same as in `alloc` call.
-pub fn free(base: usize, rank: u32) void {
+pub fn free(base: usize, rank: u8) void {
     std.debug.assert((base % vm.page_size) == 0 and rank < max_rank);
 
     var page_base: u32 = @truncate(base / vm.page_size);
@@ -170,7 +161,7 @@ pub fn free(base: usize, rank: u32) void {
     defer allocated_pages -= @as(u32, 1) << @truncate(rank);
 
     if (getPageBit(page_base, rank) == 0 or rank == max_rank - 1) {
-        const entry = makeNode(page_base);
+        const entry = nodeFromPage(page_base);
         free_areas[rank].list.prepend(entry);
 
         setPageBit(page_base, rank);
@@ -178,7 +169,6 @@ pub fn free(base: usize, rank: u32) void {
     }
 
     var temp_rank = rank;
-
     while (getPageBit(page_base, temp_rank) != 0 and temp_rank < max_rank - 1) {
         const rank_pages = @as(u32, 1) << @truncate(temp_rank);
 
@@ -195,7 +185,7 @@ pub fn free(base: usize, rank: u32) void {
         clearPageBit(buddy_base, temp_rank);
 
         const list = &free_areas[temp_rank].list;
-        const entry = getNode(buddy_base);
+        const entry = nodeFromPage(buddy_base);
 
         list.remove(entry);
 
@@ -203,7 +193,7 @@ pub fn free(base: usize, rank: u32) void {
         temp_rank += 1;
     }
 
-    const new_node = makeNode(page_base);
+    const new_node = nodeFromPage(page_base);
     free_areas[temp_rank].list.prepend(new_node);
 
     setPageBit(page_base, temp_rank);
@@ -237,7 +227,7 @@ fn initAreas(bitmap_base: usize, bitmap_size: u32) void {
     // Initialize bitmaps
     for (0..max_areas) |i| {
         const bits: [*]u8 = @ptrFromInt(curr_bitmap_base);
-        free_areas[i].bitmap = utils.Bitmap.init(bits[0..curr_bitmap_size], true);
+        free_areas[i].bitmap = .init(bits[0..curr_bitmap_size], true);
 
         curr_bitmap_base += curr_bitmap_size;
         curr_bitmap_size = @max((curr_bitmap_size >> 1) + (curr_bitmap_size & 1), 1);
@@ -262,17 +252,15 @@ fn pushFreeEntry(entry: *const boot.MemMap.Entry) void {
 
     while (temp_pages != 0) {
         var temp_rank: u32 = math.log2_int(u32, temp_pages);
-
         if (temp_rank >= max_rank) temp_rank = max_rank - 1;
 
         var rank_pages_num: u32 = @as(u32, 1) << @truncate(temp_rank);
-
         while ((temp_base % rank_pages_num) != 0) {
             temp_rank -= 1;
             rank_pages_num >>= 1;
         }
 
-        const node = makeNode(temp_base);
+        const node = nodeFromPage(temp_base);
         free_areas[temp_rank].list.prepend(node);
 
         temp_base += rank_pages_num;
@@ -284,15 +272,7 @@ fn pushFreeEntry(entry: *const boot.MemMap.Entry) void {
 /// by physical base.
 /// 
 /// - `phys_base`: Index of the first physical page in a linear block.
-inline fn getNode(phys_base: u32) *Node {
-    return makeNode(phys_base);
-}
-
-/// Returns `Node` related to the physical pages block located
-/// by physical base.
-/// 
-/// - `phys_base`: Index of the first physical page in a linear block.
-inline fn makeNode(phys_base: u32) *Node {
+inline fn nodeFromPage(phys_base: u32) *Node {
     const phys_addr = @as(usize, phys_base) * vm.page_size;
     const node_addr = vm.getVirtLma(phys_addr);
 
@@ -300,29 +280,29 @@ inline fn makeNode(phys_base: u32) *Node {
 }
 
 /// Gets the base address of a free node.
-inline fn entGetBase(node: *Node) u32 {
-    return @truncate(entGetPhys(node) / vm.page_size);
+inline fn getPageNodeBase(node: *Node) u32 {
+    return @truncate(getPageNodePhys(node) / vm.page_size);
 }
 
 /// Gets the physical address of a free node.
 ///
 /// @export
-inline fn entGetPhys(node: *Node) usize {
+inline fn getPageNodePhys(node: *Node) usize {
     return vm.getPhysLma(@intFromPtr(node));
 }
 
-inline fn clearPageBit(base: u32, rank: u32) void {
-    free_areas[rank].bitmap.clear(base >> @truncate(1 + rank));
+inline fn clearPageBit(base: u32, rank: u8) void {
+    free_areas[rank].bitmap.clear(base >> @truncate(1 +% rank));
 }
 
-inline fn setPageBit(base: u32, rank: u32) void {
-    free_areas[rank].bitmap.set(base >> @truncate(1 + rank));
+inline fn setPageBit(base: u32, rank: u8) void {
+    free_areas[rank].bitmap.set(base >> @truncate(1 +% rank));
 }
 
-inline fn getPageBit(base: u32, rank: u32) u8 {
-    return free_areas[rank].bitmap.get(base >> @truncate(1 + rank));
+inline fn getPageBit(base: u32, rank: u8) u8 {
+    return free_areas[rank].bitmap.get(base >> @truncate(1 +% rank));
 }
 
-inline fn togglePageBit(base: u32, rank: u32) void {
-    free_areas[rank].bitmap.toggle(base >> @truncate(1 + rank));
+inline fn togglePageBit(base: u32, rank: u8) void {
+    free_areas[rank].bitmap.toggle(base >> @truncate(1 +% rank));
 }
